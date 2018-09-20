e90170829717a616cbe370a577bce5bc18bd4d84
ali.abdoli <ali.abdoli@just-eat.com>
2018-09-20 16:54:37 +1000

    Sherlock and cost

---
+++ AlgorithmHackerrank.Tests/DynamicProgramming/SherlockAndCostTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmHackerrank.DynamicProgramming;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AlgorithmHackerrank.Tests.DynamicProgramming
+{
+    [TestFixture]
+    public class SherlockAndCostTests
+    {
+        [Test]
+        [TestCase(@"1
+5
+100 2 100 2 100", 396)]
+        public void SherlockAndCostWhenThen(string input, int expected)
+        {
+            var reader = new StringReader(input);
+
+            int t = Convert.ToInt32(reader.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(reader.ReadLine());
+
+                int[] B = Array.ConvertAll(reader.ReadLine().Split(' '), BTemp => Convert.ToInt32(BTemp));
+                var algor = new SherlockAndCost();
+                int result = algor.cost(B);
+
+                Assert.AreEqual(expected, result);
+
+            }
+        }
+    }
+
+}
---
+++ AlgorithmHackerrank.Tests/Searching/MinumumLossTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmHackerrank.Searching;
+using NUnit.Framework;
+
+namespace AlgorithmHackerrank.Tests.Searching
+{
+    [TestFixture]
+    public class MinumumLossTests
+    {
+        [Test]
+//        [TestCase(@"3
+//5 10 3", 2)]
+        [TestCase(@"5
+20 7 8 2 5", 2)]
+        public void WhenThen(string input, int expected)
+        {
+            var reader = new StringReader(input);
+            int n = Convert.ToInt32(reader.ReadLine());
+
+            long[] price = Array.ConvertAll(reader.ReadLine().Split(' '), priceTemp => Convert.ToInt64(priceTemp));
+
+            var algorighm = new MinimumLoss();
+            int result = algorighm.minimumLoss(price);
+            
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
