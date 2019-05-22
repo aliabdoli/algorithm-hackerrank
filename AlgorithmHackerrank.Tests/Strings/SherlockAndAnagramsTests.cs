0a56ed481017d17a9a300e1e7e005e02103434c3
ali.abdoli <ali.abdoli@just-eat.com>
2019-05-23 09:32:31 +1000

    strings

---
+++ AlgorithmHackerrank.Tests/Strings/SherlockAndAnagramsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmHackerrank.Searching;
+using AlgorithmHackerrank.Strings;
+using NUnit.Framework;
+
+namespace AlgorithmHackerrank.Tests.Strings
+{
+    public class SherlockAndAnagramsTests
+    {
+        [Test]
+        [TestCase(@"2
+abba
+abcd", @"4,0")]
+        [TestCase(@"2
+kkkk
+ifailuhkqq", @"10,3")]
+        public void MainFlow(string inputString, string expectedString)
+        {
+            var expected = expectedString.Split(',');
+            var algor = new SherlockAndAnagrams();
+            var input = new StringReader(inputString);
+            int q = Convert.ToInt32(input.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = input.ReadLine();
+
+                int result = algor.sherlockAndAnagrams(s);
+                Assert.AreEqual(expected[qItr], result.ToString());
+            }
+
+        }
+    }
+}
---
+++ AlgorithmHackerrank/Strings/BearAndSteadyGene.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmHackerrank.Strings
+{
+    public class BearAndSteadyGene
+    {
+        public int steadyGene(string gene)
+        {
+            
+            var input = gene.ToCharArray();
+            var lenForEach = input.Length / 4;
+            var frequencies = input.GroupBy(x => x).ToDictionary(x => x.Key, y => lenForEach - y.Count());
+            var allowed = new List<char>()
+            {
+                'A','C','T', 'G'
+            };
+
+            foreach (var item in allowed)
+            {
+                if (!frequencies.ContainsKey(item))
+                {
+                    frequencies.Add(item, lenForEach);
+                }    
+            }
+
+            var left = 0;
+            var right = -1;
+       
+            var mores = frequencies.Where(x => x.Value < 0).ToDictionary(x => x.Key, y => y.Value);
+
+            while (right < input.Length - 1 && mores.Any(x => x.Value < 0))
+            {
+                right++;
+
+                if (mores.ContainsKey(input[right]))
+                {
+                    mores[input[right]]++;
+                }
+            }
+
+            var bestRight = right;
+            var bestLeft = left;
+
+            while (right < input.Length - 1)
+            {
+                right++;
+                if (!mores.ContainsKey(input[right]))
+                {
+                    continue;
+                }
+
+                mores[input[right]]++;
+
+                while (true)
+                {
+                    if(mores.ContainsKey(input[left]))
+                    {
+                        mores[input[left]]--;
+                        var valid = mores.All(x => x.Value >= 0);
+                        if (valid)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            mores[input[left]]++;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+
+                var isAnswer = mores.All(x => x.Value >= 0);
+                if (isAnswer && bestRight - bestLeft > right - left)
+                {
+                    bestLeft = left;
+                    bestRight = right;
+                }
+            }
+            
+            var result = bestRight - bestLeft +1;
+            
+
+            return result;
+           
+        }
+    }
+}
+
+
