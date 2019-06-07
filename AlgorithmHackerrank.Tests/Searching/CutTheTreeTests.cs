6747333a16afec003a95387598abb4f631b2988a
ali.abdoli <ali.abdoli@just-eat.com>
2019-06-07 16:53:55 +1000

    CutTheTree

---
+++ AlgorithmHackerrank.Tests/Searching/CutTheTreeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmHackerrank.Searching;
+using NUnit.Framework;
+
+namespace AlgorithmHackerrank.Tests.Searching
+{
+    public class CutTheTreeTests
+    {
+        [Test]
+        [TestCase(@"6
+100 200 100 500 100 600
+1 2
+2 3
+2 5
+4 5
+5 6", "400")]
+        public void MainFlow(string inputString, string expectedString)
+        {
+            var input = new StringReader(inputString);
+
+            int n = Convert.ToInt32(input.ReadLine().Trim());
+
+            List<int> data = input.ReadLine().TrimEnd().Split(' ').ToList().Select(dataTemp => Convert.ToInt32(dataTemp)).ToList();
+
+            List<List<int>> edges = new List<List<int>>();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                edges.Add(input.ReadLine().TrimEnd().Split(' ').ToList().Select(edgesTemp => Convert.ToInt32(edgesTemp)).ToList());
+            }
+
+            int result = CutTheTree.cutTheTree(data, edges);
+
+            Assert.AreEqual(expectedString, result.ToString());
+        }
+    }
+}
