1eb5b287ca3ff016608a00fbafb7cd07e84aca9b
Ali Abdoli <ali.abdoli.aust@gmail.com>
2019-11-18 16:21:37 +1100

    JourneyToTheMoon

---
+++ AlgorithmHackerrank.Tests/GraphTheory/JourneyToTheMoonTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmHackerrank.GraphTheory;
+using NUnit.Framework;
+
+namespace AlgorithmHackerrank.Tests.GraphTheory
+{
+    public class JourneyToTheMoonTests
+    {
+//        [TestCase(@"5 3
+//0 1
+//2 3
+//0 4", "6")]
+//        [TestCase(@"4 1
+//0 2", "5")]
+
+        [TestCase(@"100000 2
+1 2
+3 4", "4999949998")]
+        public void MainFlow(string inputString, string expectedString)
+        {
+            var inputReader = new StringReader(inputString);
+            var expectedReader = new StringReader(expectedString);
+
+            string[] np = inputReader.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(np[0]);
+
+            int p = Convert.ToInt32(np[1]);
+
+            int[][] astronaut = new int[p][];
+
+            for (int i = 0; i < p; i++)
+            {
+                astronaut[i] = Array.ConvertAll(inputReader.ReadLine().Split(' '), astronautTemp => Convert.ToInt32(astronautTemp));
+            }
+
+            var result = JourneyToTheMoon.journeyToMoon(n, astronaut);
+
+            var expected = expectedReader.ReadLine();
+
+            Assert.AreEqual(expected, result.ToString());
+        }
+    }
+}
---
+++ AlgorithmHackerrank/GraphTheory/JourneyToTheMoon.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmHackerrank.GraphTheory
+{
+    public static class JourneyToTheMoon
+    {
+        public static long journeyToMoon(int austroNum, int[][] stupidAustras)
+        {
+            var moreThanOnes = new List<int>();
+            var ones = 0;
+
+            var austras = stupidAustras.Select(x => new []
+            {
+                new {src = x[0], dest = x[1]},
+                new {src = x[1], dest = x[0]}
+            })
+                .SelectMany(x => x)
+                .GroupBy(x => x.src, y => y.dest)
+                .ToDictionary(x => x.Key, y => y.Select(z => z).ToList());
+
+            var seens = new HashSet<int>();
+            for (int i = 0; i < austroNum; i++)
+            {
+                var beforeRunCountryCount = seens.Count;
+
+                Dfs(i, austras, seens);
+                var justSeenCountryCount = seens.Count - beforeRunCountryCount;
+                if (justSeenCountryCount > 1)
+                    moreThanOnes.Add(justSeenCountryCount);
+                if(justSeenCountryCount == 1)
+                    ones++;
+            }
+
+
+
+            int moreThanOnesResult = 0;
+            for (int i = 0; i < moreThanOnes.Count; i++)
+            {
+                for (int j = i+1; j < moreThanOnes.Count; j++)
+                {
+                    moreThanOnesResult += moreThanOnes[i] * moreThanOnes[j];
+                }
+            }
+
+            //try
+            //{
+            //    checked
+            //    {
+                    long oneResult = ((long)ones * (long)(ones - 1)) / 2;
+
+
+                    long mixResult = 0;
+                    for (int i = 0; i < moreThanOnes.Count; i++)
+                    {
+                        mixResult += moreThanOnes[i] * ones;
+                    }
+                    var result = moreThanOnesResult + oneResult + mixResult;
+                    return result;
+
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw;
+            //}
+
+            //return -1;
+        }
+
+        private static void Dfs(int current, Dictionary<int, List<int>> astras, HashSet<int> seens)
+        {
+            if(!seens.Add(current))
+                return;
+
+            List<int> children;
+            if(!astras.TryGetValue(current, out children))
+                return;
+
+            foreach (var child in children)
+            {
+                Dfs(child, astras, seens);
+            }
+        }
+    }
+}
