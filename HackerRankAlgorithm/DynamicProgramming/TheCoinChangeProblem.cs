c3b764150a88fd4a5dd0f8276d66dbb95a1a1713
aliabdoli <ali.abdoli.aust@gmail.com>
2025-02-09 18:41:22 +1100

    TheCoinChangeProblem

--- HackerRankAlgorithm/DynamicProgramming/TheCoinChangeProblem.cs
+++ HackerRankAlgorithm/DynamicProgramming/TheCoinChangeProblem.cs
@@ -13,181 +13,51 @@ namespace HackerRankAlgorithm.DynamicProgramming
     public class TheCoinChangeProblem
     {
 
-        public long getWays(long n, long[] c)
+        public long getWays(int n, List<long> c)
         {
-            //var nc = c.ToList();
-            //nc.Add(0);
-            //var coins = nc.OrderBy(x => x).ToArray();
-           
-
-            //var ways = getWayRec(n, coins, 0);
-
-
-            //var oWays = ways.Select(x => x.Split('-').Select(y => long.Parse(y)).OrderBy(y => y).ToList());
-
-            //var waysN = new List<List<long>>();
-            //foreach (var way in oWays)
-            //{
-            //    if (!waysN.Any(x => x.SequenceEqual(way)))
-            //        waysN.Add(way);
-            //}
-
-            //var result = waysN.Count;
-            //return result;
-            return -1;
-        }
-
-        public List<string> getWayRec(long n, long[] coins, int cCoinInd)
-        {
-            var ways = new List<string>();
-            if (n < 0)
-                return ways;
-
-            if (n == 0)
-            {
-                ways.Add($"{coins[cCoinInd]}");
-                return ways;
-            }
-
-            for (int i = 1; i < coins.Length; i++)
-            {
-                
-                var nWays = getWayRec(n - coins[i], coins, i);
-                if (nWays.Count > 0)
-                {
-                    var toAdd = nWays.Select(x => $"{coins[cCoinInd]}-{x}");
-                    ways.AddRange(toAdd);
-                }
-            }
-
+            var calculator = new CoinChangeCalculator();
+            long ways = calculator.Count(n, c);
             return ways;
         }
+    }
 
-        public class Way
+    public class CoinChangeCalculator
+    {
+        public long Count(int totalAmount, List<long> coins)
         {
-            public List<string> Changes { get; set; }
-        }
+            var coinAmountMappings = new Dictionary<long, Dictionary<int, long>>();
 
-        public long getWays2(long n, long[] c)
-        {
-            var coins = c.OrderBy(x => x).ToArray();//.Select(x => new {value = x, way = 0}).ToArray();
-            var ways = Enumerable.Range(0, n+1).Select(x =>
+
+            var amounts = Enumerable.Range(0, totalAmount+1).Select(x => x).ToList();
+            for (int i = 0; i < coins.Count; i++)
             {
-                var item = new
+                var currentCoin = coins[i];
+                coinAmountMappings[currentCoin] = new Dictionary<int, long>();
+                foreach (var amount in amounts)
                 {
-                    Ways = new List<string>()
-                };
-                if (c.Contains(x))
-                {
-                    item.Ways.Add($"{x}");
-                }
-                return item;
-            }).ToArray();
-
-            var minCoin = coins.Min();
-            var maxCoin = coins.Max();
-            for (long i = 0; i < ways.Length;  i += minCoin )
-            {
-                if (i - maxCoin > 0)
-                {
-                    ways[i - maxCoin].Ways.Clear();
-                }
-
-                for (int j = 0; j < coins.Length; j++)
-                {
-                    if (i - coins[j] >= 0)
+                    if (amount == 0)
                     {
-                        if (ways[i - coins[j]].Ways.Count > 0)
-                        {
-                            var newChange = coins[j];
+                        //todo: new implementation
+                    }
 
-                            var otherChanges = ways[i - coins[j]].Ways.Select(x => $"{x}-{newChange}").ToList();
+                    var currentWay = 0;
+                    if (amount < currentCoin)
+                    {
+                        if (currentCoin - 1 < 0)
+                        {
                             
-                            ways[i].Ways.AddRange(otherChanges);
-
                         }
+                        //coinAmountMappings[currentCoin] = 
                     }
+                    coinAmountMappings[currentCoin][amount] = 0;
                 }
             }
 
-            //var oWays = ways[n].Ways.Select(x => x.GroupBy(y => y).OrderBy(y => y.Key).ToDictionary(z => z.Key, z => z.Count())).ToList();
-            //var oWays = ways[n].Ways.Select(y => y.OrderBy(z => z.Key).ToDictionary(x => x.Key, x => x.Frequency));
-            var oWays = ways[n].Ways.Select(x => x.Split('-').Select(y => long.Parse(y)).OrderBy(y => y).ToList());
 
-            var waysN = new List<List<long>>();
-            foreach (var way in oWays)
-            {
-                if (!waysN.Any(x => x.SequenceEqual(way)))
-                    waysN.Add(way);
-            }
-
-            var result = waysN.Count;
-
-            return result;
-        }
+            
 
 
 
-
-        public long getWays1(long n, long[] c)
-        {
-            var coins = c.OrderBy(x => x).ToArray();//.Select(x => new {value = x, way = 0}).ToArray();
-            var ways = Enumerable.Range(0, n + 1).Select(x =>
-            {
-                var item = new
-                {
-                    Changes = new HashSet<HashSet<long>>()
-                };
-                if (c.Contains(x))
-                {
-                    item.Changes.Add(new HashSet<long>(new List<long> { x }));
-                }
-                return item;
-            }).ToArray();
-
-            for (int i = 1; i < ways.Length; i++)
-            {
-                for (int j = 0; j < coins.Length; j++)
-                {
-                    if (i - coins[j] >= 0)
-                    {
-                        if (ways[i - coins[j]].Changes.Count > 0)
-                        {
-                            var newChange = coins[j];
-
-                            var otherChanges = ways[i - coins[j]].Changes.Select(x =>
-                            {
-                                var list = new HashSet<long>(x.Select(y => y));
-                                list.Add(newChange);
-                                return list;
-                            });
-
-                            foreach (var otherChange in otherChanges)
-                            {
-                                if (!ways[i].Changes.Any(x => x.SetEquals(otherChange)))
-                                    ways[i].Changes.Add(otherChange);
-                            }
-
-                        }
-                    }
-                }
-            }
-
-            var result = ways[n].Changes.Count;
-            return result;
-        }
-
-
-        public static class Enumerable
-        {
-            public static IEnumerable<long> Range(long start, long count)
-            {
-                var end = start + count;
-                for (var current = start; current < end; ++current)
-                {
-                    yield return current;
-                }
-            }
         }
     }
 }
--- README.md
+++ README.md
@@ -62,6 +62,6 @@ Here is the list of algo, I redo, i.e. again solve them [37 success, 51 total ti
 		* Inverse RMQ: [HARD] : timeout
 	* Feb 2025 
 		* HackerRank City: [Medium] : failed
-		*  
+		* TheCoinChangeProblem [Medium] [failed, cheating](https://www.youtube.com/watch?v=RjDYwJc7y84) 
 
 
